45266211aff35bad30e7e00ea497ceb92e1b6c50
z9961 <laserjet1031@gmail.com>
2019-01-13 22:56:50 +0800

    request demo

--- src/main/java/neko/controller/UsersController.java
+++ src/main/java/neko/controller/UsersController.java
@@ -4,10 +4,14 @@ package neko.controller;
 import neko.entity.Users;
 import neko.service.IUsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -26,8 +30,17 @@ public class UsersController {
     @Autowired
     private IUsersService usersService;
 
-    @RequestMapping("getall")
+    @RequestMapping("/getall")
     public List<Users> getall() {
         return usersService.list();
+    }
+
+    @RequestMapping("/login")
+    public Map<String, String> login(HttpServletRequest request, String username, String password) {
+        System.out.println(username + "\n" + password);
+        Map<String, String> map = new HashMap<>();
+        map.put("state", "200");
+        map.put("msg", "ok");
+        return map;
     }
 }
