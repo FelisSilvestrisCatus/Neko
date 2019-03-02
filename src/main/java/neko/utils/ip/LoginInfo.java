2be134fec099accbdf675d90a4511a59d5ea4b29
z9961 <laserjet1031@gmail.com>
2019-03-02 22:49:20 +0800

    Test

--- src/main/java/neko/utils/ip/LoginInfo.java
+++ src/main/java/neko/utils/ip/LoginInfo.java
@@ -27,42 +27,42 @@ public class LoginInfo {
         String ip = request.getHeader("x-forwarded-for");
         System.out.println("x-forwarded-for" + ip);
         String getRemoteIp = "";
-//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             //X-Real-IP 记录一次真实的浏览器地址
             ip = request.getHeader("X-Real-IP");
-//            System.out.println("X-Real-IP"+ip);
-//        }
-//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            System.out.println("X-Real-IP" + ip);
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("X-Forwarded-For");
-        System.out.println("X-Forwarded-For" + ip);
-//        }
+            System.out.println("X-Forwarded-For" + ip);
+        }
 
         //　X-Forwarded-For  WL-Proxy-Client-IP都是apache的请求头
-//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
-        System.out.println("Proxy-Client-IP" + ip);
-//        }
+            System.out.println("Proxy-Client-IP" + ip);
+        }
 
 
-//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
-        System.out.println("WL-Proxy-Client-IP" + ip);
-//        }
+            System.out.println("WL-Proxy-Client-IP" + ip);
+        }
 
 
-//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("HTTP_CLIENT_IP");
-        System.out.println("HTTP_CLIENT_IP" + ip);
-//        }
+            System.out.println("HTTP_CLIENT_IP" + ip);
+        }
 
 
-//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-        System.out.println("HTTP_X_FORWARDED_FOR" + ip);
-//        }
-//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            System.out.println("HTTP_X_FORWARDED_FOR" + ip);
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
-        System.out.println("getRemoteAddr" + ip);
+            System.out.println("getRemoteAddr" + ip);
             //判断是不是本地ip
             if (ip.equals("127.0.0.1") || ip.equals("0:0:0:0:0:0:0:1")) {
 
@@ -75,7 +75,7 @@ public class LoginInfo {
                 }
                 ip = inet.getHostAddress();
             }
-//        }
+        }
         if (ip != null && ip.length() > 15) { //"***.***.***.***".length() = 15  
             if (ip.indexOf(",") > 0) {
                 ip = ip.split(",")[ip.split(",").length - 1];
