51f6e60b003eb69223185eaabd6439de383034c1
LinLinLinWang <1339567630@qq.com>
2019-03-05 13:27:24 +0800

    使用redis保存验证码

--- src/main/java/neko/controller/UsersregisteController.java
+++ src/main/java/neko/controller/UsersregisteController.java
@@ -7,6 +7,8 @@ import neko.entity.Userslogin;
 import neko.service.IUsersService;
 import neko.service.IUsersloginService;
 import neko.utils.ip.LoginInfo;
+import neko.utils.message.Message;
+import neko.utils.redis.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,45 +37,60 @@ public class UsersregisteController {
     private IUsersService userslService;
     @Autowired
     private LoginInfo loginInfo;
-    userphone
+    //使用工具类
+    @Autowired
+    private Message message;
+    //使用redis
+    private RedisUtil edisUtil;
+
     @RequestMapping(value = "/phoneIsOrNotExist")
     public Map<String, String> phoneIsOrNotExist(HttpServletRequest request) {
-        String userphone=request.getParameter("userphone");
+        String userphone = request.getParameter("userphone");
         Map<String, String> map = new HashMap<>();
-        System.out.println("用户输入手机号后接受手机号"+userphone);
+        System.out.println("用户输入手机号后接受手机号" + userphone);
         //查询该手机号用户是否存在
         QueryWrapper queryWrapper = new QueryWrapper();
-        queryWrapper.eq("phone",userphone);
+        queryWrapper.eq("phone", userphone);
 
-        List<Users>  userlist= userslService.listObjs(queryWrapper);
-        System.out.println("拥有该手机号的用户有"+userlist.size()+"个");
-        if(userlist.size()==0){
+        List<Users> userlist = userslService.listObjs(queryWrapper);
+        System.out.println("拥有该手机号的用户有" + userlist.size() + "个");
+        if (userlist.size() == 0) {
             System.out.println("拥有该手机号的用户有0个");
-            map.put("state","0");
-        }else{
-            map.put("state","1");
+            map.put("state", "0");
+        } else {
+            map.put("state", "1");
 
         }
         return map;
     }
+
     @RequestMapping(value = "/getValidatecode")
     public Map<String, String> getValidatecode(HttpServletRequest request) {
-        String userphone=request.getParameter("userphone");
+        String userphone = request.getParameter("userphone");
         Map<String, String> map = new HashMap<>();
-        System.out.println("用户输入手机号后接受手机号"+userphone);
+        System.out.println("获取验证码的手机号" + userphone);
         //查询该手机号用户是否存在
-        QueryWrapper queryWrapper = new QueryWrapper();
-        queryWrapper.eq("phone",userphone);
+        map = message.getCode(userphone);
+        //获取手机号成功
+        if (map.get("state").equalsIgnoreCase("0")) {
+            //获取验证码存入redis
+            System.out.println("控制层获取的验证码" + map.get("state"));
 
-        List<Users>  userlist= userslService.listObjs(queryWrapper);
-        System.out.println("拥有该手机号的用户有"+userlist.size()+"个");
-        if(userlist.size()==0){
-            System.out.println("拥有该手机号的用户有0个");
-            map.put("state","0");
-        }else{
-            map.put("state","1");
+            edisUtil.set("userphone", map.get("state"));
 
+            try {
+                edisUtil.set("userphone", map.get("state"));
+                map.put("state","ok");
+            } catch (Exception e) {
+                map.put("state","false");
+
+            }
+
+
+        } else {
+
+            map.put("state","false");
         }
-        return map;
+return map;
     }
 }
