fbb7c99ede70bb024aaa57af9815a7de5439b2bd
LinLinLinWang <1339567630@qq.com>
2019-03-10 11:22:07 +0800

    修改班级添加以及教师id的绑定

--- src/main/java/neko/controller/ClassController.java
+++ src/main/java/neko/controller/ClassController.java
@@ -2,10 +2,10 @@ package neko.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import neko.entity.*;
 import neko.entity.Class;
-import neko.entity.Users;
-import neko.entity.Vacate;
 import neko.service.IClassService;
+import neko.service.IClassteacherService;
 import neko.service.IUsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,32 +30,50 @@ public class ClassController {
     private IUsersService userslService;
     @Autowired
     private IClassService classService;
+    @Autowired
+    private IClassteacherService classteacherServic;
+
     @RequestMapping(value = "/createClass")
     public Map<String, String> getValidatecode(HttpServletRequest request, String name) {
 
         Map<String, String> map = new HashMap<>();
         Users users = (Users) request.getSession().getAttribute("user");
 
-        if(users.getFlag()!=0){
+        if (users.getFlag() != 0) {
             //用户状态不合法 ： 或权限不够 或状态不正常
-            map.put("state","401");
-            map.put("msg","用户状态不合法(疑似注销)");
-            return  map;
+            map.put("state", "401");
+            map.put("msg", "用户状态不合法(疑似注销)");
+            return map;
 
-        } if(users.getType()!=1){
+        }
+        if (users.getType() != 1) {
             //用户状态不合法 ： 或权限不够 或状态不正常
-            map.put("state","401");
-            map.put("msg","用户权限不够");
-            return  map;
+            map.put("state", "401");
+            map.put("msg", "用户权限不够");
+            return map;
         }
 
-             //创建班级表
-            Class  newclass=new Class();
-            newclass.setCname(name);
-            classService.save(newclass);
-            map.put("state","200");
-            map.put("msg","创建成功");
-            return map;
+
+
+        //创建班级表
+        Class newclass = new Class();
+        newclass.setCname(name);
+        classService.save(newclass);
+
+        //老师绑定课程
+
+        QueryWrapper queryWrapper = new QueryWrapper();
+
+        queryWrapper.orderByAsc("cid");
+
+        Classteacher classteacher = new Classteacher();
+
+        classteacher.setCid((((Class)classService.list(queryWrapper).get(classService.count()-1)).getCid()));
+        classteacher.setUid(users.getUid());
+        classteacherServic.save(classteacher);
+        map.put("state", "200");
+        map.put("msg", "创建成功");
+        return map;
 
 
     }
