f83feec4b43d82cdc3a665d0b00fbd7e8c18909f
z9961 <449014690@qq.com>
2019-03-27 21:04:52 +0800

    查询学生的课程

---
+++ src/main/java/neko/entity/Rollcalldetails.java
@@ -0,0 +1,48 @@
+package neko.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author z9961
+ * @since 2019-03-23
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class Rollcalldetails implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 自增主键
+     */
+    @TableId(value = "rdid", type = IdType.AUTO)
+    private Integer rdid;
+
+    /**
+     * 点名id
+     */
+    private Integer rid;
+
+    /**
+     * 学生id
+     */
+    private Integer uid;
+
+    /**
+     * 0出勤,1缺勤
+     */
+    private Integer state;
+
+
+}
