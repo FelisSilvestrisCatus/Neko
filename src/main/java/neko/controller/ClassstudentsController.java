8e77732ec50b74212a1168fec45d41ab35445004
LinLinLinWang <1339567630@qq.com>
2019-04-20 23:25:41 +0800

    完善了好多东西我就不一一写了 有点累

--- src/main/java/neko/controller/ClassstudentsController.java
+++ src/main/java/neko/controller/ClassstudentsController.java
@@ -5,20 +5,20 @@ import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import neko.entity.Class;
 import neko.entity.Classstudents;
+import neko.entity.Classteacher;
 import neko.entity.Users;
 import neko.entity.vo.ClassWithTeacherName;
 import neko.service.IClassService;
 import neko.service.IClassstudentsService;
 import neko.service.IClassteacherService;
+import neko.service.IUsersService;
 import neko.utils.generalMethod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * <p>
@@ -37,6 +37,8 @@ public class ClassstudentsController {
     private IClassteacherService classteacherServic;
     @Autowired
     private IClassstudentsService classstudentsService;
+    @Autowired
+    private IUsersService usersService;
 
     //加入班级
     @RequestMapping(value = "/joinClass")
@@ -82,5 +84,85 @@ public class ClassstudentsController {
         map.put("msg", "ok");
         map.put("data", JSON.toJSONString(classes));
         return map;
+    }
+
+    //获取已加入班级的学生
+    @RequestMapping(value = "/getStudents")
+    public Map<String, String> getJoinedStudent(HttpSession session, String cid, String state) {
+        int cid_ = Integer.valueOf(cid);
+        int flag_ = Integer.valueOf(state);
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("cid", cid_);
+        queryWrapper.eq("state", flag_);
+        Map<String, String> map = new HashMap<>();
+        Users users = (Users) session.getAttribute("user");
+        List<Classstudents> list = classstudentsService.list(queryWrapper);
+        System.out.println("学生数=" + list.size());
+        Iterator it = list.iterator();
+        List<Integer> uid_list = new ArrayList<>();
+        while (it.hasNext()) {
+            int uid = ((Classstudents) it.next()).getUid();
+            uid_list.add(uid);
+        }
+        if (uid_list.size() == 0) {
+            map.put("data", JSON.toJSONString(null));
+        } else {
+            QueryWrapper queryWrapper1 = new QueryWrapper();
+            queryWrapper1.in("uid", uid_list);
+            List<Users> usersList = usersService.list(queryWrapper1);
+            map.put("data", JSON.toJSONString(usersList));
+
+
+        }
+
+        return map;
+    }
+
+    //删除指定班级id 指定uid的学生
+    @RequestMapping(value = "/deleteStudent")
+    public Map<String, String> deleteStudent(HttpSession session, String cid, String uid) {
+        int cid_ = Integer.valueOf(cid);
+        int uid_ = Integer.valueOf(uid);
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("cid", cid_);
+        queryWrapper.eq("uid", uid_);
+        Map<String, String> map = generalMethod.getSuccessMap();
+        if (classstudentsService.remove(queryWrapper)) {
+            return map;
+        } else {
+            map = generalMethod.getErrorMap();
+            return map;
+        }
+
+
+    }
+
+    //同意学生加入该班级的申请
+    @RequestMapping(value = "/agreeApply")
+    public Map<String, String> agreeApply(HttpSession session, String cid, String uid) {
+        int cid_ = Integer.valueOf(cid);
+        int uid_ = Integer.valueOf(uid);
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("cid", cid_);
+        queryWrapper.eq("uid", uid_);
+        List<Classstudents> list = classstudentsService.list(queryWrapper);
+
+        Map<String, String> map = generalMethod.getSuccessMap();
+        Iterator it = list.iterator();
+        List<Classstudents> list_state = new ArrayList<>();
+        Classstudents classstudents = new Classstudents();
+        while (it.hasNext()) {
+            classstudents = ((Classstudents) it.next()).setState(1);
+            list_state.add(classstudents);
+        }
+        try {
+            classstudentsService.updateBatchById(list_state);
+            return map;
+        } catch (Exception e) {
+            map = generalMethod.getErrorMap();
+            return map;
+        }
+
+
     }
 }
