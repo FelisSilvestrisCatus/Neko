d02d6213b029ed0d5a4029a0fc99f00e1401442d
z9961 <449014690@qq.com>
2019-04-20 22:41:11 +0800

    添加 usermessage

---
+++ src/main/java/neko/service/IUsermessageService.java
@@ -0,0 +1,16 @@
+package neko.service;
+
+import neko.entity.Usermessage;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author z9961
+ * @since 2019-04-20
+ */
+public interface IUsermessageService extends IService<Usermessage> {
+
+}
