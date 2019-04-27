e465064a288aefdc278a80c4d186eef9a6d40a95
z9961 <449014690@qq.com>
2019-04-27 22:24:28 +0800

    查看个人统计信息v2

--- src/main/java/neko/service/impl/RollcalldetailsServiceImpl.java
+++ src/main/java/neko/service/impl/RollcalldetailsServiceImpl.java
@@ -6,14 +6,17 @@ import neko.entity.Rollcalldetails;
 import neko.entity.Users;
 import neko.entity.vo.ClassWithTeacherName;
 import neko.entity.vo.StudentCourseName;
+import neko.entity.vo.VacateWithTeacherName;
 import neko.mapper.RollcalldetailsMapper;
 import neko.service.IClassService;
 import neko.service.ICourseService;
 import neko.service.IRollcalldetailsService;
+import neko.service.IVacateService;
 import neko.utils.generalMethod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,7 +36,8 @@ public class RollcalldetailsServiceImpl extends ServiceImpl<RollcalldetailsMappe
     private IClassService classService;
     @Autowired
     private ICourseService courseService;
-
+    @Autowired
+    private IVacateService vacateService;
 
     @Override
     public Map<String, String> myInfo(Users users) {
@@ -41,17 +45,32 @@ public class RollcalldetailsServiceImpl extends ServiceImpl<RollcalldetailsMappe
 
         //存放需要的数据
         Map<String, String> data = new HashMap<>();
+        List<Map<String, String>> datalist = new ArrayList<>();
+
 
         //加入的班级
         List<ClassWithTeacherName> classes = classService.getJoinedclass(users.getUid());
-        data.put("我加入的班级", classes.size() + "");
+        data = new HashMap<>();
+        data.put("name", "我加入的班级");
+        data.put("number", classes.size() + "");
+        datalist.add(data);
 
         //我的课程
         List<StudentCourseName> course = courseService.getMyCourse(users.getUid());
-        data.put("我的课程", course.size() + "");
+        data = new HashMap<>();
+        data.put("name", "我的课程");
+        data.put("number", course.size() + "");
+        datalist.add(data);
+
+        //我的请假
+        List<VacateWithTeacherName> vacates = vacateService.getMyVacate(users.getUid());
+        data = new HashMap<>();
+        data.put("name", "我的请假");
+        data.put("number", vacates.size() + "");
+        datalist.add(data);
 
 
-        map.put("data", JSON.toJSONString(data));
+        map.put("data", JSON.toJSONString(datalist));
         return map;
     }
 }
