31aed92375daaaa3dc505d7ea92b0b0eba8ec544
z9961 <449014690@qq.com>
2019-05-08 20:25:18 +0800

    fix login bugs

--- src/main/java/neko/controller/GlobalExceptionHandler.java
+++ src/main/java/neko/controller/GlobalExceptionHandler.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 /*
@@ -28,39 +27,39 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ShiroException.class)
     @ResponseBody
-    public Map<String, String> handleShiroException(HttpServletResponse response) {
-        Map<String, String> map = get401Map(response);
+    public Map<String, String> handleShiroException() {
+        Map<String, String> map = get401Map();
         map.put("msg", "鉴权或授权过程出错");
         return map;
     }
 
     @ExceptionHandler(UnauthenticatedException.class)
     @ResponseBody
-    public Map<String, String> UnauthenticatedException(HttpServletResponse response) {
-        Map<String, String> map = get401Map(response);
+    public Map<String, String> UnauthenticatedException() {
+        Map<String, String> map = get401Map();
         map.put("msg", "认证失败");
         return map;
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     @ResponseBody
-    public Map<String, String> UnauthorizedException(HttpServletResponse response) {
-        Map<String, String> map = get401Map(response);
+    public Map<String, String> UnauthorizedException() {
+        Map<String, String> map = get401Map();
         map.put("msg", "授权失败");
         return map;
     }
 
     @ExceptionHandler(LockedAccountException.class)
     @ResponseBody
-    public Map<String, String> LockedAccountException(HttpServletResponse response) {
-        Map<String, String> map = get401Map(response);
+    public Map<String, String> LockedAccountException() {
+        Map<String, String> map = generalMethod.getErrorMap();
         map.put("msg", "该账号已被禁用");
         return map;
     }
 
     @ExceptionHandler(IncorrectCredentialsException.class)
     @ResponseBody
-    public Map<String, String> IncorrectCredentialsException(HttpServletResponse response) {
+    public Map<String, String> IncorrectCredentialsException() {
         Map<String, String> map = generalMethod.getErrorMap();
         map.put("msg", "用户名或密码错误");
         return map;
@@ -68,15 +67,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(UnknownAccountException.class)
     @ResponseBody
-    public Map<String, String> UnknownAccountException(HttpServletResponse response) {
-        Map<String, String> map = get401Map(response);
+    public Map<String, String> UnknownAccountException() {
+        Map<String, String> map = generalMethod.getErrorMap();
         map.put("msg", "未注册的用户");
         return map;
     }
 
 
-    private Map<String, String> get401Map(HttpServletResponse response) {
-//        response.setStatus(401);
+    private Map<String, String> get401Map() {
         Map<String, String> map = generalMethod.getErrorMap();
         map.put("state", "401");
         return map;
