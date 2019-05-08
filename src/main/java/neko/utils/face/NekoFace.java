c1760eb6653ff60158a207acea0e2c281a4b5adb
LinLinLinWang <1339567630@qq.com>
2019-05-08 14:53:22 +0800

    人脸识别接口

--- src/main/java/neko/utils/face/NekoFace.java
+++ src/main/java/neko/utils/face/NekoFace.java
@@ -75,4 +75,61 @@ public class NekoFace {
 
         return jsonObject;
     }
+    //调用接口返回人脸识别的结果
+    public JSONObject faceRecognition(String temppath, String uid) {
+
+        JSONObject jsonObject = new JSONObject();
+
+        // 获得Http客户端
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        // 参数
+        StringBuffer params = new StringBuffer();
+        try {
+            // 字符数据最好encoding以下;这样一来，某些特殊字符才能传过去(如:某人的名字就是“&”,不encoding的话,传不过去)
+            params.append("temppath=" + URLEncoder.encode(temppath, "utf-8"));
+            params.append("&");
+            params.append("uid=" + URLEncoder.encode(uid, "utf-8"));
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        // 创建Post请求
+        HttpPost httpPost = new HttpPost("http://localhost:58886/faceRecognition" + "?" + params);
+
+        // 设置ContentType(注:如果只是传普通参数的话,ContentType不一定非要用application/json)
+        httpPost.setHeader("Content-Type", "application/json;charset=utf8");
+
+        // 响应模型
+        CloseableHttpResponse response = null;
+        try {
+            // 由客户端执行(发送)Post请求
+            response = httpClient.execute(httpPost);
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+
+            if (responseEntity != null) {
+                String resdata = EntityUtils.toString(responseEntity);
+                System.out.println("resdata = " + resdata);
+                jsonObject = JSON.parseObject(resdata);
+            }
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 释放资源
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return jsonObject;
+    }
 }
