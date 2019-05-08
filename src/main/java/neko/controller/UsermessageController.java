9da2b0596d1c1dc2906cabbd00a3fe0e293d219c
z9961 <449014690@qq.com>
2019-05-08 15:06:38 +0800

    未读消息数量添加redis缓存

--- src/main/java/neko/controller/UsermessageController.java
+++ src/main/java/neko/controller/UsermessageController.java
@@ -5,12 +5,14 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import neko.entity.Users;
 import neko.service.IUsermessageService;
 import neko.utils.generalMethod;
+import neko.utils.redis.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -27,16 +29,36 @@ public class UsermessageController {
     @Autowired
     private IUsermessageService usermessage;
 
+    @Autowired
+    private RedisUtil redisUtil;
+
+    //Redis过期时间
+    private static final long tokenExpire = 12;
+    //Redis过期时间单位
+    private static final TimeUnit tokenExpireTimeUnit = TimeUnit.HOURS;
+
+
     //获取未读消息数量
     @RequestMapping(value = "/getUnReadNum")
     public Map<String, String> getUnReadNum(HttpSession session) {
         Users u = (Users) session.getAttribute("user");
         Map<String, String> map = generalMethod.getSuccessMap();
-        QueryWrapper queryWrapper = new QueryWrapper();
-        queryWrapper.eq("uid", u.getUid());
-        queryWrapper.eq("mstate", 0);
-        Integer count = usermessage.count(queryWrapper);
-        map.put("data", count.toString());
+        int count = 0;
+
+        String key = "Message:" + u.getUid();
+        //检查redis中是否有缓存
+        if (redisUtil.hasKey(key)) {
+            count = Integer.parseInt(redisUtil.get(key));
+        } else {
+            QueryWrapper queryWrapper = new QueryWrapper();
+            queryWrapper.eq("uid", u.getUid());
+            queryWrapper.eq("mstate", 0);
+            count = usermessage.count(queryWrapper);
+            redisUtil.set(key, Integer.toString(count));
+            redisUtil.expire(key, tokenExpire, tokenExpireTimeUnit);
+        }
+        map.put("data", Integer.toString(count));
+
         return map;
     }
 
@@ -49,12 +71,16 @@ public class UsermessageController {
     @RequestMapping(value = "/setMsgRead")
     public Map<String, String> setMsgRead(HttpSession session, int umid) {
         Users u = (Users) session.getAttribute("user");
+        String key = "Message:" + u.getUid();
+        redisUtil.delete(key);
         return usermessage.setMsgRead(u.getUid(), umid);
     }
 
     @RequestMapping(value = "/setAllMsgRead")
     public Map<String, String> setAllMsgRead(HttpSession session) {
         Users u = (Users) session.getAttribute("user");
+        String key = "Message:" + u.getUid();
+        redisUtil.delete(key);
         return usermessage.setAllMsgRead(u.getUid());
     }
 }
