56e3bdc0b0aa431965677056c03069f63a1b884d
LinLinLinWang <1339567630@qq.com>
2019-05-18 13:00:38 +0800

    添加分页

--- src/main/java/neko/mapper/ClassstudentsMapper.java
+++ src/main/java/neko/mapper/ClassstudentsMapper.java
@@ -1,7 +1,14 @@
 package neko.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import neko.entity.Classstudents;
+import neko.entity.vo.StudentRollcall;
+import org.apache.ibatis.annotations.Param;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +19,9 @@ import neko.entity.Classstudents;
  * @since 2019-02-28
  */
 public interface ClassstudentsMapper extends BaseMapper<Classstudents> {
-
+    //(HttpSession session, Integer currentPage, String cid, String state)
+    IPage<Classstudents> getJoinedStudent(Page page, @Param("currentPage") Integer currentPage,
+                                          @Param("cid") String cid,
+                                          @Param("state") String state,
+                                          @Param("uid_list") List<Integer> uid_list);
 }
--- src/main/java/neko/service/impl/ClassstudentsServiceImpl.java
+++ src/main/java/neko/service/impl/ClassstudentsServiceImpl.java
@@ -1,10 +1,15 @@
 package neko.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import neko.entity.Classstudents;
+import neko.entity.vo.StudentRollcall;
 import neko.mapper.ClassstudentsMapper;
 import neko.service.IClassstudentsService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +21,9 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ClassstudentsServiceImpl extends ServiceImpl<ClassstudentsMapper, Classstudents> implements IClassstudentsService {
-
+    @Override
+    public IPage<Classstudents> getJoinedStudent(Integer currentPage, String cid, String state, List<Integer> uid_list) {
+        Page<StudentRollcall> page = new Page<>(currentPage, 10);
+        return this.baseMapper.getJoinedStudent(page, currentPage, cid, state,uid_list);
+    }
 }
